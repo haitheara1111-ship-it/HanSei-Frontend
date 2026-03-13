c7571b5dc17db35c452618e9c4fd81e409c17d87
dariyatum <dariyatum412@gmail.com>
2026-03-13 08:08:09 +0700

    set up layout

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,13 @@
 
 <template>
   <div class="flex-center">
-      <div class="flex-col">
-  <Header />
-  <NuxtPage />
-  </div>
-  <DashboardLayout />
-
+    <div class="flex-col center">
+        <Header />
+      <div class="flex">
+        <DashboardLayout />
+        <NuxtPage />
+      </div>
+    </div>
   </div>
 
 </template>
