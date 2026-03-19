cdf7c09b88e00ec5e5304563b4d2ece9deb6ef62
haitheara <haitheara1111@gmail.com>
2026-03-19 16:07:49 +0700

    feat:rename the path link in sidebar from attedance to HR-page.

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -80,7 +80,7 @@ const menus = reactive([
     submenu: [
       { name: "Admin Dashboard", path: "/" },
       { name: "Employee Dashboard", path: "/Employee-Dashboard" },
-      { name: "Attendance Dashboard", path: "/Attendence", label: "New" },
+      { name: "Attendance Dashboard", path: "/HR-page", label: "New" },
       { name: "Roster Dashboard", path: "/Roster", label: "New" },
       { name: "Jobs", path: "/JobsListing", label: "" },
       { name: "Leave Management", path: "/Leave", label: "" },
